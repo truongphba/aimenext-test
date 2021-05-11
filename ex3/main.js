c391340a32b40d38020ae47d84a51dc471bc0685
truongphba <truongph.ba@gmail.com>
2021-05-12 02:22:29 +0700

    finish ex3

--- ex3/main.js
+++ ex3/main.js
@@ -5,16 +5,22 @@ buttons.forEach((item) => {
     item.onclick = () => {
         display.classList.remove('animated');
         if (item.id === 'clear') {
-            display.innerText = '';
+            display.innerText = '0';
         } else if (item.id === 'backspace') {
             let string = display.innerText.toString();
-            display.innerText = string.substr(0, string.length - 1);
+            if ( display.innerText.length < 2 ){
+                display.innerText = '0'
+            } else {
+                display.innerText = string.substr(0, string.length - 1);
+            }
         } else if ((item.id === 'equal' && calculations.includes(display.innerText[display.innerText.length - 1])) || (display.innerText === '' && item.id === 'equal')) {
             display.classList.add('animated')
         } else if (display.innerText !== '' && item.id === 'equal') {
             display.innerText = eval(display.innerText);
             display.classList.add('animated')
-        } else if (calculations.includes(item.id) && calculations.includes(display.innerText[display.innerText.length - 1])) {
+        } else if ((calculations.includes(item.id) && calculations.includes(display.innerText[display.innerText.length - 1]))
+            || ((display.innerText[0]  === '0' || (calculations.includes(display.innerText[display.innerText.length - 2]) && display.innerText[display.innerText.length - 1] === '0')) && item.id !== '.' && display.innerText[1] !== '.')
+            || (item.id === '.' && display.innerText[display.innerText.length - 1] === '.')) {
             display.innerText = display.innerText.substring(0, display.innerText.length - 1) + item.id;
         } else {
             display.innerText += item.id;
